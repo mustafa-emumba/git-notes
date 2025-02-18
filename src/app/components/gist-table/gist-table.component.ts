4d4e4f19e1d88d298ae60153cb95e01861692aa9
Mustafa Shah <mustafa.shah@emumba.com>
2025-02-18 14:46:12 +0500

    Added paginator for showing list of gists

--- src/app/components/gist-table/gist-table.component.ts
+++ src/app/components/gist-table/gist-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, AfterViewInit, SimpleChanges } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-gist-table',
@@ -8,12 +10,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GistTableComponent implements OnInit {
   @Input() gists: any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   displayedColumns: string[] = ['name', 'notebook', 'keyword', 'updatedAt', 'actions'];
   gitIconPath: string = "/icons/git.svg";
   starIconPath: string = "/icons/star.svg";
+  dataSource = new MatTableDataSource<any>([]);
 
   ngOnInit(): void {
-    console.log(this.gists)
+    if (this.gists && this.gists.length > 0) {
+      this.loadData(this.gists);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['gists'] && this.gists.length > 0) {
+      this.loadData(this.gists);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadData(gists: any[]) {
+    this.dataSource.data = gists;
   }
 
   getFileName(file: any): string {
@@ -25,7 +45,7 @@ export class GistTableComponent implements OnInit {
     const now = new Date();
     const then = new Date(timestamp);
     const seconds = Math.round((now.getTime() - then.getTime()) / 1000);
-  
+
     const intervals = [
       { label: 'year', seconds: 31536000 },
       { label: 'month', seconds: 2592000 },
@@ -34,14 +54,14 @@ export class GistTableComponent implements OnInit {
       { label: 'minute', seconds: 60 },
       { label: 'second', seconds: 1 }
     ];
-  
+
     for (const interval of intervals) {
       const count = Math.floor(seconds / interval.seconds);
       if (count >= 1) {
-        return `Last updated ${count} ${interval.label}${count > 1? 's': ''} ago`;
+        return `Last updated ${count} ${interval.label}${count > 1 ? 's' : ''} ago`;
       }
     }
-  
+
     return 'Last updated just now';
   }
 
